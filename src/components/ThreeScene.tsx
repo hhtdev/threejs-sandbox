6ae6849381c777617c86a7658e58c04d00cc8067
Hugo Herault <hugo.herault-ext@francetravail.fr>
2024-11-08 10:22:10 +0100

    Light and shadow

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -6,30 +6,47 @@ const ThreeScene: React.FC = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
   const geometry = new THREE.SphereGeometry();
   const texture = new THREE.TextureLoader().load("earth.jpg");
-  const meshMaterial = new THREE.MeshBasicMaterial({ map: texture });
+  const meshMaterial = new THREE.MeshStandardMaterial({ map: texture });
   const planet = new THREE.Mesh(geometry, meshMaterial);
   const controls = new OrbitControls(camera, renderer.domElement);
-
+  const dirLight = new THREE.DirectionalLight(0xffffff, 1.8);
+  const ambientLight = new THREE.AmbientLight(0x404040);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.shadowMap.enabled = true;
       containerRef.current?.appendChild(renderer.domElement);
+
+      // Camera distance from planet
       camera.position.z = 5;
+
+      // Add inertia
       controls.enableDamping = true;
       controls.dampingFactor = 0.05;
 
+      // Lock camera rotation to the y axis
+      controls.minPolarAngle = Math.PI / 2;
+      controls.maxPolarAngle = Math.PI / 2;
+
+      // Planet inclination
+      planet.rotation.x = 0.23;
+
+      // Enable shadows
+      planet.castShadow = true;
+      planet.receiveShadow = true;
+      dirLight.castShadow = true;
+
+      // Add light
+      dirLight.position.set(50, 0, 30);
+      scene.add(dirLight);
+      scene.add(ambientLight);
       scene.add(planet);
 
       animate();
-
-      controls.addEventListener('change', () => {
-        renderer.render(scene, camera)
-      });
-
 
       window.addEventListener('resize', handleResize);
       // Clean up the event listener when the component is unmounted
@@ -60,4 +77,5 @@ const ThreeScene: React.FC = () => {
     <div ref={containerRef} />
   );
 };
+
 export default ThreeScene;
