294fc5d3d1a76995a1b42ed87fbe5f70bef4960d
Hugo Herault <hugo.herault-ext@francetravail.fr>
2024-11-08 14:24:11 +0100

    background

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -7,15 +7,20 @@ const ThreeScene: React.FC = () => {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer({ antialias: true });
-  const geometry = new THREE.SphereGeometry(1, 32, 32);
+  const planetGeometry = new THREE.SphereGeometry(1, 64, 64);
+  const planetAtmosphereGeometry = new THREE.SphereGeometry(1.01, 64, 64);
+  const starBackgroundGeometry = new THREE.SphereGeometry(90, 128, 128);
   const earthTexture = new THREE.TextureLoader().load("earth.jpg");
   const earthCloudsTexture = new THREE.TextureLoader().load("earth-clouds.png");
-  const materialEarth = new THREE.MeshStandardMaterial({ map: earthTexture, depthTest: false });
+  const starsTexture = new THREE.TextureLoader().load("8k-stars-milky-way.jpg");
+  const materialEarth = new THREE.MeshStandardMaterial({ map: earthTexture, depthTest: true });
   const materialClouds = new THREE.MeshStandardMaterial({ map: earthCloudsTexture, transparent: true, depthTest: false });
-  const earthMesh = new THREE.Mesh(geometry, materialEarth);
-  earthMesh.renderOrder = 0;
-  const earthCloudsMesh = new THREE.Mesh(geometry, materialClouds);
-  earthCloudsMesh.renderOrder = 1;
+  const materialAtmosphere = new THREE.MeshStandardMaterial({ color: 0x00b3ff, emissive: 0x00b3ff, transparent: true, opacity: 0.1 });
+  const materialStars = new THREE.MeshBasicMaterial({ map: starsTexture, side: THREE.BackSide });
+  const earthMesh = new THREE.Mesh(planetGeometry, materialEarth);
+  const earthCloudsMesh = new THREE.Mesh(planetGeometry, materialClouds);
+  const earthAtmosphereMesh = new THREE.Mesh(planetAtmosphereGeometry, materialAtmosphere);
+  const starBackgroundMesh = new THREE.Mesh(starBackgroundGeometry, materialStars);
   const controls = new OrbitControls(camera, renderer.domElement);
   const dirLight = new THREE.DirectionalLight(0xffffff, 1.8);
   const ambientLight = new THREE.AmbientLight(0x404040);
@@ -41,9 +46,16 @@ const ThreeScene: React.FC = () => {
       // controls.minDistance = 5;
       // controls.maxDistance = 5;
 
+      //Planet mesh render order
+      earthMesh.renderOrder = 0;
+      earthCloudsMesh.renderOrder = 1;
+
       // Planet inclination
       earthMesh.rotation.x = 0.23;
       earthCloudsMesh.rotation.x = 0.23;
+
+      //Milky way inclination
+      starBackgroundMesh.rotation.x = 60.2;
 
       // Enable shadows
       earthMesh.castShadow = true;
@@ -60,6 +72,8 @@ const ThreeScene: React.FC = () => {
       scene.add(ambientLight);
       scene.add(earthMesh);
       scene.add(earthCloudsMesh);
+      scene.add(earthAtmosphereMesh);
+      scene.add(starBackgroundMesh);
 
       animate();
 
